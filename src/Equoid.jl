8efdda0832311ba04c3b7ff4877ab4524af86928
Erik Edin <erikedin@users.noreply.github.com>
2022-11-05 12:47:16 +0100

    create window using GLFW

--- src/Equoid.jl
+++ src/Equoid.jl
@@ -1,5 +1,27 @@
 module Equoid
 
-greet() = print("Hello World!")
+using GLFW
+
+function run()
+    # Create a window and its OpenGL context
+    window = GLFW.CreateWindow(640, 480, "Equoid")
+
+    # Make the window's context current
+    GLFW.MakeContextCurrent(window)
+
+    # Loop until the user closes the window
+    while !GLFW.WindowShouldClose(window)
+
+	    # Render here
+
+	    # Swap front and back buffers
+	    GLFW.SwapBuffers(window)
+
+	    # Poll for and process events
+	    GLFW.PollEvents()
+    end
+
+    GLFW.DestroyWindow(window)
+end
 
 end # module Equoid
