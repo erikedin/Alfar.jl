2eda6c2513abd1a8972030530df28b3befee60e9
Erik Edin <erikedin@users.noreply.github.com>
2023-06-11 13:23:32 +0200

    visualization: create an OpenGL window in a worker process

--- src/Visualization/Visualization.jl
+++ src/Visualization/Visualization.jl
@@ -15,28 +15,69 @@
 module Visualization
 
 using GLFW
+using ModernGL
 
-function start()
+using Distributed
+
+@everywhere using Alfar.Visualization
+
+abstract type VizEvent end
+
+struct ExitEvent <: VizEvent end
+
+function handle(window, e::ExitEvent)
+    GLFW.SetWindowShouldClose(window, true)
+end
+
+function runvisualizer(c::RemoteChannel)
     # Create a window and its OpenGL context
     window = GLFW.CreateWindow(640, 480, "Alfar Visualization")
 
     # Make the window's context current
     GLFW.MakeContextCurrent(window)
 
+    # Set background color to black
+    glClearColor(0.0f0, 0.0f0, 0.0f0, 1.0f0)
+
     # Loop until the user closes the window
     while !GLFW.WindowShouldClose(window)
+        glClear(GL_COLOR_BUFFER_BIT)
 
 	    # Render here
 
 	    # Swap front and back buffers
 	    GLFW.SwapBuffers(window)
 
+        # If we have any events from the REPL, handle them.
+        hasevents = isready(c)
+        if hasevents
+            ev = take!(c)
+            handle(window, ev)
+        end
+
 	    # Poll for and process events
 	    GLFW.PollEvents()
     end
 
     GLFW.DestroyWindow(window)
+end
 
+struct VisualizationContext
+    channel::RemoteChannel
+end
+
+function start()
+    channel = RemoteChannel(() -> Channel{Visualization.VizEvent}(10))
+
+    workerpid = Distributed.workers()[1]
+
+    remote_do(Visualization.runvisualizer, workerpid, channel)
+
+    VisualizationContext(channel)
+end
+
+function stop(context::VisualizationContext)
+    put!(context.channel, ExitEvent())
 end
 
 end
