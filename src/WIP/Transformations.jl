582ce430140d445c39f5b5bf3e355515795a0b00
Erik Edin <erikedin@users.noreply.github.com>
2023-07-22 10:31:18 +0200

    math: implement some quaternion math

--- src/WIP/Transformations.jl
+++ src/WIP/Transformations.jl
@@ -18,15 +18,50 @@ using Alfar.WIP.Math
 
 export PointRotation, transform
 
+struct Quaternion{T}
+    e0::T
+    ei::T
+    ej::T
+    ek::T
+end
+
+function complement(q::Quaternion{T}) where {T}
+    Quaternion{T}(q.e0, -q.ei, -q.ej, -q.ek)
+end
+
+function Base.:*(q::Quaternion{T}, p::Quaternion{T}) where {T}
+    e0 = q.e0 * p.e0 - q.ei * p.ei - q.ej * p.ej - q.ek * p.ek
+    ei = q.e0 * p.ei + q.ei * p.e0 + q.ej * p.ek - q.ek * p.ej
+    ej = q.e0 * p.ej + q.ej * p.e0 + q.ek * p.ei - q.ei * p.ek
+    ek = q.e0 * p.ek + q.ek * p.e0 + q.ei * p.ej - q.ej * p.ei
+    Quaternion{T}(e0, ei, ej, ek)
+end
+
 # PointRotation is a rotation that rotates a vector `v` in the same
 # coordinate system that it originates in.
 struct PointRotation{T, System}
-    Θ::T
+    θ::T
     axis::Vector3{T, System}
 end
 
-function transform(::PointRotation{T, System}, v::Vector3{T, System}) where {T, System}
-    v
+function transform(r::PointRotation{T, System}, v::Vector3{T, System}) :: Vector3{T, System} where {T, System}
+    # This defines the quaternion that will rotate the vector `v`.
+    q = Quaternion{Float32}(
+        cos(r.θ / 2),
+        sin(r.θ / 2) * r.axis.x, 
+        sin(r.θ / 2) * r.axis.y, 
+        sin(r.θ / 2) * r.axis.z, 
+    )
+    # ... along with its complement.
+    qstar = complement(q)
+    # Reinterpret `v` as a pure quaternion (with zero for its scalar value)
+    vq = Quaternion{T}(zero(T), v.x, v.y, v.z)
+    # This is the actual rotation.
+    result = q * vq * qstar
+    # This reinterprets the resulting quaternion as a vector.
+    # This is assumed to be a pure quaternion, but we can make a debug assert here
+    # if we want.
+    Vector3{T, System}(result.ei, result.ej, result.ek)
 end
 
 # FrameRotation rotates a vector `v` from one coordinate system to another.
--- test/math/rotation_test.jl
+++ test/math/rotation_test.jl
@@ -34,6 +34,18 @@ R = TestCoordinateSystem2
     @test result ≈ Vector3{Float32, S}(1.0, 2.0, 3.0)
 end
 
+@testset "A point rotation of (0, 1, 0) pi/2 radians around the X axis; vector is (0, 0, 1)" begin
+    # Arrange
+    v = Vector3{Float32, S}(0f0, 1f0, 0f0)
+    rotation = PointRotation{Float32, S}(0.5f0 * pi, Vector3{Float32, S}(1f0, 0f0, 0f0))
+
+    # Act
+    result = transform(rotation, v)
+
+    # Assert
+    @test result ≈ Vector3{Float32, S}(0f0, 0f0, 1f0)
+end
+
 @testset "A point rotation for system S is applied to system R; MethodError" begin
     # Arrange
     v = Vector3{Float32, R}(1.0, 2.0, 3.0)
@@ -51,4 +63,48 @@ end
     # Act and assert
     @test_throws MethodError transform(rotation, v)
 end
+
+#
+# Table driven tests for point rotations
+#
+
+point_rotation_test_cases = [
+    ("Rotate pi/2 X around Z -> Y",                  # Description
+     Vector3{Float32, S}(1f0, 0f0, 0f0),             # Vector to rotate
+     0.5f0 * pi, Vector3{Float32, S}(0f0, 0f0, 1f0), # Angle and axis around which to rotate
+     Vector3{Float32, S}(0f0, 1f0, 0f0)),            # Result vector
+
+    ("Rotate pi/2 Y around X -> Z",
+     Vector3{Float32, S}(0f0, 1f0, 0f0),
+     0.5f0 * pi, Vector3{Float32, S}(1f0, 0f0, 0f0),
+     Vector3{Float32, S}(0f0, 0f0, 1f0)),
+
+    ("Rotate pi/2 Z around Y -> X",
+     Vector3{Float32, S}(0f0, 0f0, 1f0),
+     0.5f0 * pi, Vector3{Float32, S}(0f0, 1f0, 0f0),
+     Vector3{Float32, S}(1f0, 0f0, 0f0)),
+
+    ("Rotate pi/2 Y around Z -> -X",
+     Vector3{Float32, S}(0f0, 1f0, 0f0),
+     0.5f0 * pi, Vector3{Float32, S}(0f0, 0f0, 1f0),
+     Vector3{Float32, S}(-1f0, 0f0, 0f0)),
+]
+
+for testcase in point_rotation_test_cases
+    @testset "$(testcase[1])" begin
+        # Arrange
+        v = testcase[2]
+        angle = testcase[3]
+        axis = testcase[4]
+        expected = testcase[5]
+        rotation = PointRotation{Float32, S}(angle, axis)
+
+        # Act
+        result = transform(rotation, v)
+
+        # Assert
+        @test result ≈ expected
+    end
+end
+
 end
