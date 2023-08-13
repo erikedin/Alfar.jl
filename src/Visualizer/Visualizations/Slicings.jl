3754cde114adedb90f22a261c48295e29a9f8c72
Erik Edin <erikedin@users.noreply.github.com>
2023-08-13 10:12:58 +0200

    slicing: render a single slice in slicing

--- src/Visualizer/Visualizations/Slicings.jl
+++ src/Visualizer/Visualizations/Slicings.jl
@@ -19,16 +19,121 @@ using ModernGL
 using Alfar.Visualizer
 using Alfar.Rendering.Cameras
 using Alfar.Rendering.CameraViews
+using Alfar.Rendering.Shaders
+using Alfar.Rendering.Meshs
 using Alfar.Rendering.Inputs
 using Alfar.Rendering: World
 using Alfar.Visualizer.Objects.Boxs
 using Alfar.WIP.Math
 
+function frontvertex(cameraview::CameraView) :: Int
+    vertices = Vector3{Float32, World}[
+        Vector3{Float32, World}( 0.5f0,  0.5f0,  0.5f0), #v0
+        Vector3{Float32, World}( 0.5f0,  0.5f0, -0.5f0), #v1
+        Vector3{Float32, World}( 0.5f0, -0.5f0,  0.5f0), #v2
+        Vector3{Float32, World}(-0.5f0,  0.5f0,  0.5f0), #v3
+        Vector3{Float32, World}(-0.5f0,  0.5f0, -0.5f0), #v4
+        Vector3{Float32, World}( 0.5f0, -0.5f0, -0.5f0), #v5
+        Vector3{Float32, World}(-0.5f0, -0.5f0,  0.5f0), #v6
+        Vector3{Float32, World}(-0.5f0, -0.5f0, -0.5f0), #v7
+    ]
+
+    frontvertex = 0
+    frontdistance = Inf
+
+    for vertexindex = 1:8
+        v = vertices[vertexindex]
+        d = norm(cameraposition(cameraview) - v)
+
+        if d < frontdistance
+            frontdistance = d
+            frontvertex = vertexindex
+        end
+
+    end
+
+    # Convert from Julias one-indexing to OpenGLs zero-indexing
+    frontvertex - 1
+end
+
+
+struct IntersectingPolygon
+    program::ShaderProgram
+    polygon::VertexArray{GL_TRIANGLE_FAN}
+    color::NTuple{4, Float32}
+
+    function IntersectingPolygon(color::NTuple{4, Float32})
+        program = ShaderProgram("shaders/visualization/vs_polygon_intersecting_box.glsl",
+                                "shaders/visualization/uniformcolorfragment.glsl")
+
+        # Instead of specifying actually vertices, or even vertex indexes to be looked up,
+        # here we specify the intersection points that will make up the polygon.
+        # There will be between 3-6 intersections, with intersection p0, p2, p4 being guaranteed.
+        # This is a triangle fan, originating at intersection p0.
+        intersectionindexes = GLint[
+            0, 1, 2,
+               2, 3,
+               3, 4,
+               4, 5,
+        ]
+
+        indexattribute = VertexAttribute(0, 1, GL_INT, GL_FALSE, C_NULL)
+        indexdata = VertexData{GLint}(intersectionindexes, VertexAttribute[indexattribute])
+
+        polygon = VertexArray{GL_TRIANGLE_FAN}(indexdata)
+
+        new(program, polygon, color)
+    end
+end
+
+function render(polygon::IntersectingPolygon,
+                camera::Camera,
+                cameraview::CameraView,
+                normalcameraview::CameraView,
+                distance::Float32,
+                frontvertexindex::Int)
+    use(polygon.program)
+
+    projection = perspective(camera)
+    model = identitytransform()
+    view = CameraViews.lookat(cameraview)
+
+    uniform(polygon.program, "projection", projection)
+    uniform(polygon.program, "view", view)
+    uniform(polygon.program, "model", model)
+    uniform(polygon.program, "distance", distance)
+    uniform(polygon.program, "color", polygon.color)
+    uniform(polygon.program, "frontVertexIndex", frontvertexindex)
+
+    # We define the slice to have a positive normal on its front facing side.
+    # Since the slices should always be oriented to show their front facing sides to the camera,
+    # it implies that the normal is the direction.
+    normal = -direction(normalcameraview)
+    uniform(polygon.program, "normal", normal)
+
+    renderarray(polygon.polygon)
+end
+
+struct Slices
+    polygon::IntersectingPolygon
+
+    function Slices()
+        polygon = IntersectingPolygon((0f0, 1f0, 0f0, 1f0))
+        new(polygon)
+    end
+end
+
+function render(slices::Slices, camera::Camera, cameraview::CameraView, normalcameraview::CameraView, n::Int)
+    frontvertexindex = frontvertex(normalcameraview)
+    render(slices.polygon, camera, cameraview, normalcameraview, 0f0, frontvertexindex)
+end
+
 struct Slicing <: Visualizer.Visualization
     box::Box
+    slices::Slices
 
     function Slicing()
-        new(Box())
+        new(Box(), Slices())
     end
 end
 
@@ -76,6 +181,8 @@ function Visualizer.render(camera::Camera, slicing::Slicing, state::SlicingState
     glViewport(0, 0, camera.windowwidth, camera.windowheight)
 
     Boxs.render(slicing.box, camera, state.cameraview)
+
+    render(slicing.slices, camera, state.cameraview, state.cameraview, state.numberofslices)
 end
 
 end # module Slicings
