efef3fc3550bd52b5b6152066da5a1d34670cbfb
Erik Edin <erikedin@users.noreply.github.com>
2023-08-13 09:51:06 +0200

    visualizer: slicing shows a box

--- src/Visualizer/Visualizations/Slicings.jl
+++ src/Visualizer/Visualizations/Slicings.jl
@@ -14,31 +14,71 @@
 
 module Slicings
 
+using ModernGL
+
 using Alfar.Visualizer
 using Alfar.Rendering.Cameras
+using Alfar.Rendering.CameraViews
+using Alfar.Rendering.Inputs
+using Alfar.Rendering: World
+using Alfar.Visualizer.Objects.Boxs
+using Alfar.WIP.Math
 
 struct Slicing <: Visualizer.Visualization
+    box::Box
 
+    function Slicing()
+        new(Box())
+    end
 end
 
 struct SlicingState <: Visualizer.VisualizationState
     numberofslices::Int
+    cameraview::CameraView
 end
 
 function Visualizer.setflags(::Slicing)
-
+    glEnable(GL_BLEND)
+    glEnable(GL_DEPTH_TEST)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+    glDisable(GL_CULL_FACE)
 end
 
 function Visualizer.setup(::Slicing) :: SlicingState
-    SlicingState(5)
+    position = Vector3{Float32, World}(0f0, 0f0, 3f0)
+    target = Vector3{Float32, World}(0f0, 0f0, 0f0)
+    up = Vector3{Float32, World}(0f0, 1f0, 0f0)
+    cameraview = CameraView{Float32, World}(position, target, up)
+    SlicingState(5, cameraview)
 end
 
 function Visualizer.update(::Slicing, state::SlicingState) :: SlicingState
     state
 end
 
-function Visualizer.render(::Camera, ::Slicing, ::SlicingState)
+function Visualizer.onmousedrag(::Slicing, state::SlicingState, ev::MouseDragStartEvent)
+    newcameraview = onmousedrag(state.cameraview, ev)
+    SlicingState(state.numberofslices, newcameraview)
+end
 
+function Visualizer.onmousedrag(::Slicing, state::SlicingState, ev::MouseDragEndEvent)
+    newcameraview = onmousedrag(state.cameraview, ev)
+    SlicingState(state.numberofslices, newcameraview)
+end
+
+function Visualizer.onmousedrag(::Slicing, state::SlicingState, ev::MouseDragPositionEvent)
+    newcameraview = onmousedrag(state.cameraview, ev)
+    SlicingState(state.numberofslices, newcameraview)
+end
+
+function Visualizer.render(camera::Camera, slicing::Slicing, state::SlicingState)
+    # TODO: Temporary hard coding of the front vertex until the code is in a common place
+    frontvertexindex = 1
+
+    # Viewport 1 (left)
+    glViewport(0, 0, camera.windowwidth, camera.windowheight)
+
+    Boxs.render(slicing.box, camera, state.cameraview, frontvertexindex)
 end
 
 end # module Slicings
