85564ff4362ff43c761ebabcefe0f9fad1e0b9d9
Erik Edin <erikedin@users.noreply.github.com>
2023-08-13 09:53:47 +0200

    visualizer: re-insert the colored Box in ViewportAlignment
    The generic box should be uniformly colored and can be simpler.

--- src/Visualizer/Visualizations/ViewportAlignmentAlgorithm.jl
+++ src/Visualizer/Visualizations/ViewportAlignmentAlgorithm.jl
@@ -20,7 +20,6 @@ using GLFW
 using Alfar.Visualizer
 using Alfar.Visualizer: MouseDragStartEvent, MouseDragEndEvent, MouseDragPositionEvent
 using Alfar.Visualizer.Objects.XYZMarkerObject
-using Alfar.Visualizer.Objects.Boxs
 using Alfar.Rendering.Cameras
 using Alfar.Rendering.Inputs
 using Alfar.Rendering.CameraViews
@@ -256,6 +255,89 @@ function makewireframetexture() :: TextureData{1}
     TextureData{1}(flattransfer, width)
 end
 
+struct Box
+    program::ShaderProgram
+    boxwireframe::VertexArray{GL_LINES}
+
+    function Box()
+        program = ShaderProgram("shaders/visualization/vs_colored_box.glsl",
+                                "shaders/visualization/fragmentcolorfromvertex.glsl")
+
+        # These vertex indices define all edges that are drawn, that
+        # make up the box. Depending on which the front vertex is, the
+        # edges will have different colors though. Instead of defining
+        # that here, a translation is done in the vertex shader.
+        boxvertices = GLint[
+            0, 1, # RED path 1
+            1, 4, # RED path 2
+            4, 7, # RED path 3
+            1, 5, # Dashed RED path
+            0, 2, # GREEN path 1
+            2, 5, # GREEN path 2
+            5, 7, # GREEN path 3
+            2, 6, # Dashed GREEN path
+            0, 3, # BLUE path 1
+            3, 6, # BLUE path 2
+            6, 7, # BLUE path 3
+            3, 4, # Dash BLUE path
+        ]
+        linecolors = GLfloat[
+            1f0, 0f0, 0f0, 1f0, # RED
+            1f0, 0f0, 0f0, 1f0, # RED
+            1f0, 0f0, 0f0, 1f0, # RED
+            1f0, 0f0, 0f0, 1f0, # RED
+            1f0, 0f0, 0f0, 1f0, # RED
+            1f0, 0f0, 0f0, 1f0, # RED
+            0.5f0, 0f0, 0f0, 1f0, # RED, darker
+            0.5f0, 0f0, 0f0, 1f0, # RED, darker
+            0f0, 1f0, 0f0, 1f0, # GREEN
+            0f0, 1f0, 0f0, 1f0, # GREEN
+            0f0, 1f0, 0f0, 1f0, # GREEN
+            0f0, 1f0, 0f0, 1f0, # GREEN
+            0f0, 1f0, 0f0, 1f0, # GREEN
+            0f0, 1f0, 0f0, 1f0, # GREEN
+            0f0, 0.5f0, 0f0, 1f0, # GREEN, darker
+            0f0, 0.5f0, 0f0, 1f0, # GREEN, darker
+            0f0, 0f0, 1f0, 1f0, # BLUE
+            0f0, 0f0, 1f0, 1f0, # BLUE
+            0f0, 0f0, 1f0, 1f0, # BLUE
+            0f0, 0f0, 1f0, 1f0, # BLUE
+            0f0, 0f0, 1f0, 1f0, # BLUE
+            0f0, 0f0, 1f0, 1f0, # BLUE
+            0f0, 0f0, 0.5f0, 1f0, # BLUE, darker
+            0f0, 0f0, 0.5f0, 1f0, # BLUE, darker
+        ]
+
+        indexattribute = VertexAttribute(0, 1, GL_INT, GL_FALSE, C_NULL)
+        indexdata = VertexData{GLint}(boxvertices, VertexAttribute[indexattribute])
+
+        colorattribute = VertexAttribute(1, 4, GL_FLOAT, GL_FALSE, C_NULL)
+        colordata = VertexData{GLfloat}(linecolors, VertexAttribute[colorattribute])
+
+        boxwireframe = VertexArray{GL_LINES}(indexdata, colordata)
+
+        new(program, boxwireframe)
+    end
+end
+
+function render(box::Box, camera::Camera, cameraview::CameraView, frontvertexindex::Int)
+    use(box.program)
+
+    projection = perspective(camera)
+    model = identitytransform()
+    view = CameraViews.lookat(cameraview)
+
+    uniform(box.program, "projection", projection)
+    uniform(box.program, "view", view)
+    uniform(box.program, "model", model)
+    # frontvertexindex is 1-indexed, because it's from Julia code, but should be
+    # zero-indexed in the vertex shader.
+    uniform(box.program, "frontVertexIndex", frontvertexindex - 1)
+
+    renderarray(box.boxwireframe)
+end
+
+
 struct ViewportAlignment <: Visualizer.Visualization
     plane::IntersectingPlane
     planepoints::IntersectingPlanePoints
@@ -391,7 +473,7 @@ function Visualizer.render(camera::Camera, v::ViewportAlignment, state::Viewport
     #
     glViewport(0, 0, camera.windowwidth, camera.windowheight)
 
-    Boxs.render(v.box, camera, state.cameraview, frontvertex)
+    render(v.box, camera, state.cameraview, frontvertex)
 
     XYZMarkerObject.render(v.marker, camera, state.cameraview)
 
@@ -411,7 +493,7 @@ function Visualizer.render(camera::Camera, v::ViewportAlignment, state::Viewport
     #
     glViewport(camera.windowwidth, 0, camera.windowwidth, camera.windowheight)
 
-    Boxs.render(v.box, camera, state.fixedcameraview, frontvertex)
+    render(v.box, camera, state.fixedcameraview, frontvertex)
 
     XYZMarkerObject.render(v.marker, camera, state.fixedcameraview)
 
